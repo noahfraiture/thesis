2629a368506af5e8e70119e5e0eed2c94cdb2839
Noah <pro@noahcode.dev>
2025-06-14 15:49:05 +0200

    Add flake

---
+++ scenario-hotel/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+
+        buildInputs = with pkgs; [
+        ];
+
+        packages = with pkgs; [
+          gcc
+        ];
+
+        DIRENV = "env";
+      };
+    };
+}
